696b57a886021417a1f59bed5d61c67dcbffc6d4
willis18 <willis18@naver.com>
2021-03-18 19:48:54 +0900

    취준생을 위한 아이폰 앱개발 스토리보드 커스텀 팝업 다이얼로그 웹뷰

--- CustomPopup_Tutorial/CustomPopupViewController.swift
+++ CustomPopup_Tutorial/CustomPopupViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class CustomPopupViewController : UIViewController{
+    
+    @IBOutlet var contentView: UIView!
+    @IBOutlet var subscribeBtn: UIButton!
+    @IBOutlet var bgBtn: UIButton!
+    var subscribeBtnCompletionClosuer: (()->Void)?
     override func viewDidLoad() {
         super.viewDidLoad()
+        contentView.layer.cornerRadius = 30
+        subscribeBtn.layer.cornerRadius = 10
+         
+    }
+    @IBAction func onBgBtnClicked(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    @IBAction func onSubscribeBtnClicked(_ sender: UIButton) {
+        print("컴플레션 블록 홏")
+        self.dismiss(animated: true, completion: nil)
+        //컴플레션 블록 호출
+        if let subscribeBtnCompletionClosuer = subscribeBtnCompletionClosuer{
+            //메인에 알림
+            subscribeBtnCompletionClosuer()
+        }
     }
 }
--- CustomPopup_Tutorial/ViewController.swift
+++ CustomPopup_Tutorial/ViewController.swift
@@ -6,14 +6,36 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var createPopupBtn: UIButton!
+    @IBOutlet var myWebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onCreatePopupBtnClicked(_ sender: UIButton) {
+        //스토리보드 가져오기
+        let storyboard = UIStoryboard.init(name: "Popup", bundle: nil)
+        //스토리보드를 통해 뷰컨트롤러 가져오기
+        let customPopupVC = storyboard.instantiateViewController(withIdentifier: "AlertPopupVC") as! CustomPopupViewController
+        //뷰컨트롤러 보여지는 스타일
+        customPopupVC.modalPresentationStyle = .overCurrentContext
+        //뷰컨트롤러 사라지는 스타일
+        customPopupVC.modalTransitionStyle = .crossDissolve
+        
+        customPopupVC.subscribeBtnCompletionClosuer = {
+            let myChannelUrl = URL(string: "https://www.youtube.com/watch?v=RSGfF0qMf4o&list=PLgOlaPUIbynqRzpQBIdEDnjDdkVsjHqxK&index=19")
+            print("웹로딩 거침")
+            self.myWebView.load(URLRequest(url: myChannelUrl! ))
+        }
+        
+        self.present(customPopupVC, animated: true, completion: nil)
+    }
+    
 }
 
